33ef245df26531b02d9e7d6caffd1d1d4051641c
Thomas Stridsman <optisizer@gmail.com>
2017-11-17 12:04:56 +0100

    Filter storms data

--- 5_clean.R
+++ 5_clean.R
@@ -9,4 +9,6 @@ storms <- storms %>%
   mutate(time = ymd_h(paste(year, month, day, hour))) %>% 
   select(name, year, time, lat, long, pressure, wind, type)
 
+storms <- storms %>% filter(type != "Extratropical")
+
 write.csv(storms, file = "5_storms.csv", row.names = FALSE)
